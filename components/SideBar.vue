2022f0c1b44b43f827f4bf7a7174c8ee329928fe
SapphireGaze <jasminehuang2048@outlook.com>
2023-12-22 03:37:17 -0800

    Reworked Sidebar Component:  - Reworked sidebar component with nuxt icon  - Created settings page

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,19 +1,58 @@
+<script>
+export default {
+  data() {
+    return {
+      sidebarCollapsed: false,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.sidebarCollapsed = !this.sidebarCollapsed;
+    },
+  },
+};
+</script>
+
 <template>
-  <div class="sticky flex h-full w-72 justify-center bg-[#11101d]">
-    <div class="m-6 font-mono text-emerald-600">
-      <div class="text-5xl font-semibold">ville</div>
-      <div class="m-6 text-xl">
+  <div
+    class="sticky flex h-full w-72 justify-center bg-[#11101d] outline outline-[#131516]"
+  >
+    <div
+      class="mt-12 bg-gradient-to-br from-emerald-800 to-teal-200 bg-clip-text p-8 font-mono text-transparent"
+    >
+      <div class="text-5xl font-semibold hover:animate-pulse">ville</div>
+      <div class="-ml-4 mt-12 text-xl">
         <NuxtLink to="/">
-          <div class="p-1">Home</div>
+          <div
+            class="rounded-xl p-2 hover:animate-pulse hover:bg-[#131516] hover:font-extrabold hover:text-emerald-400 hover:outline-double"
+          >
+            <Icon name="humbleicons:home" color="#059669" size="24" />
+            Home
+          </div>
         </NuxtLink>
         <NuxtLink to="/library">
-          <div class="p-1">Library</div>
+          <div
+            class="rounded-xl p-2 hover:animate-pulse hover:bg-[#131516] hover:font-extrabold hover:text-emerald-400 hover:outline-double"
+          >
+            <Icon name="humbleicons:music-note" color="#059669" size="24" />
+            Library
+          </div>
         </NuxtLink>
         <NuxtLink to="/upload">
-          <div class="p-1">Upload</div>
+          <div
+            class="rounded-xl p-2 hover:animate-pulse hover:bg-[#131516] hover:font-extrabold hover:text-emerald-400 hover:outline-double"
+          >
+            <Icon name="humbleicons:upload" color="#059669" size="24" />
+            Upload
+          </div>
         </NuxtLink>
         <NuxtLink to="/settings">
-          <div class="p-1">Settings</div>
+          <div
+            class="mt-80 rounded-xl p-2 hover:animate-pulse hover:bg-[#131516] hover:font-extrabold hover:text-emerald-400 hover:outline-double"
+          >
+            <Icon name="healthicons:ui-settings" color="#059669" size="24" />
+            Settings
+          </div>
         </NuxtLink>
       </div>
     </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,4 +14,5 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  modules: ["nuxt-icon"],
 });
